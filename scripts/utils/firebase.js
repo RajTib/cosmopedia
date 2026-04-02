7cab87e3e49952e6dd4114d6e5a789d2450f8ab6
Astryx <raj.tibarewala@gmail.com>
2026-04-02 16:05:13 +0530

    Removed exposed API key

--- scripts/utils/firebase.js
+++ scripts/utils/firebase.js
@@ -1,21 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { firebaseConfig } from "./config.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyChnMPSNK3x5v4GWxmQeWlizehG55gpFqY",
-    authDomain: "cosmopedia-b1962.firebaseapp.com",
-    projectId: "cosmopedia-b1962",
-    storageBucket: "cosmopedia-b1962.firebasestorage.app",
-    messagingSenderId: "279732595727",
-    appId: "1:279732595727:web:002ee31949b5bf660b477a",
-    measurementId: "G-003640R6BK"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
